c0e906566d0d4f2f635c7f6beeb7a6723bab458b
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-24 16:51:36 -0300

    equalSumChecker.java

---
+++ src/methodsLearning/equalSumChecker.java
@@ -0,0 +1,17 @@
+public class EqualSumChecker {
+
+  /*Write a method hasEqualSum with 3 parameters of type int.
+  The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. 
+  Otherwise, return false.*/
+  
+  public static boolean hasEqualSum(int n1, int n2, int n3) {
+        
+        if (n1 + n2 == n3) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+}
