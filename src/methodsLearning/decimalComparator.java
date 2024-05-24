3d433a492699e563b5508c8003382ed631094040
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-24 16:49:52 -0300

    decimalComparator.java

---
+++ src/methodsLearning/decimalComparator.java
@@ -0,0 +1,16 @@
+public class DecimalComparator {
+  
+    /*Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.*/
+  
+    public static boolean areEqualByThreeDecimalPlaces(double n1, double n2) {
+        
+        if ((long)(n1 * 1000) == (long)(n2 * 1000)) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+}
