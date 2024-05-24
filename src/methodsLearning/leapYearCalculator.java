8a986f412708ea0943006f1f771dfdaa630ad909
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-24 16:48:07 -0300

    Create leapYearCalculator.java

---
+++ src/methodsLearning/leapYearCalculator.java
@@ -0,0 +1,25 @@
+public class LeapYear {
+    /*Write a method isLeapYear with a parameter of type int named year.
+The parameter needs to be greater than or equal to 1 and less than or equal to 9999. If the parameter is not in that range return false.
+Otherwise, if it is in the valid range, calculate if the year is a leap year and return true if it is a leap year, otherwise return false.
+
+A leap year is a year that is divisible by 4 but not 100.
+If it's divisible by 100, it has to be divisible by 400.*/
+  
+    public static boolean isLeapYear(int year) {
+        
+        if (year < 1 || year > 9999) {
+            return false;
+        } else {
+            if (year % 4 == 0 && year % 100 != 0) {
+                return true;
+            } else if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+    }
+    
+}
