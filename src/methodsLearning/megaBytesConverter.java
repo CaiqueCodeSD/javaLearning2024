1bfa5f60180c02bdca9cc5eead915a09d0152176
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-24 16:41:25 -0300

    Add files via upload

---
+++ src/methodsLearning/megaBytesConverter.java
@@ -0,0 +1,18 @@
+package methodsLearning;
+
+public class megaBytesConverter {
+
+	public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        
+        int megabytes = kiloBytes / 1024;
+        int remainingKilobytes = kiloBytes % 1024;
+        
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKilobytes + " KB");
+        }
+        
+    }
+    
+}
---
+++ src/practicing/calcularPercentual.java
@@ -0,0 +1,46 @@
+package practicing;
+
+/*Desenvolver um programa que leia a idade de um conjunto de pessoas. Este programa
+deverá calcular e mostrar: o percentual de pessoas adultas (&gt;=18) e o percentual de pessoas
+idosas (&gt;=65). Finalize a leitura de dados com uma idade negativa.*/
+
+import java.util.Scanner;
+
+public class calcularPercentual {
+
+	public static void main(String[] args) {
+		
+		Scanner age = new Scanner(System.in);
+		
+		int idade = 0;
+		int cont = 0;
+		int adultos = 0;
+		int idosos = 0;
+		
+		
+		while (idade >= 0) {
+			System.out.print("Idade: ");
+			idade = age.nextInt();
+			
+			if (idade >= 18) {
+				adultos++;
+				if (idade >= 65) {
+					idosos++;
+				}
+			}
+			cont++;
+			if (idade < 0) {
+				cont--;
+			}
+			
+		}
+		
+		double percentualAdultos = ((double)adultos/cont)*100;
+		double percentualIdosos = ((double)idosos/cont)*100;
+		
+		System.out.println("Percentual de pessoas adultas: " + percentualAdultos + "%");
+		System.out.println("Percentual de pessoas idosas: " + percentualIdosos + "%");
+		
+	}
+	
+}
