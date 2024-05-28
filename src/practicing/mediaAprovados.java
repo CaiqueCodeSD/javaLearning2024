8ab734a32d9179d4b6c446483e28c3547c1ba052
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-28 19:18:44 -0300

    mediaAprovados.java

---
+++ src/practicing/mediaAprovados.java
@@ -0,0 +1,18 @@
+public class mediaAprovados {
+
+
+  
+/*Ler um vetor M com as médias finais de uma turma de 8 alunos. A seguir, calcule a média
+aritmética dos alunos aprovados (média maior ou igual a 6). Armazene em um vetor A, os índices
+que cada aluno ocupa no vetor M dos alunos que obtiveram média maior ou igual que a média da
+turma.*/
+
+    public static void main (String[] args) {
+
+        int[] M = new int[8];
+
+        
+      
+    }
+  
+}
