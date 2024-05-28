a2c19ee64b38dd28861d13991a001352752522ab
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-28 20:52:47 -0300

    calculadoraTMB.java

--- src/practicing/calculadoraTMB.java
+++ src/practicing/calculadoraTMB.java
@@ -19,30 +19,30 @@ public class calculadoraTMB {
         Scanner read = new Scanner(System.in);
 
         System.out.println("CALCULADORA TMB");
-        System.out.println("Qual seu sexo? [M/F] -> ");
+        System.out.print("Qual seu sexo? [M/F] -> ");
         char sexo = read.next().charAt(0);
         char sexoMaiusc = Character.toUpperCase(sexo);
-
-        System.out.println("Informe seu peso em KG -> ")
-        double peso = read.nextDouble;
-
-        if (sexoMaiusc != 'M' || sexoMaiusc != 'F') {
+        
+        if ((sexoMaiusc != 'M') && (sexoMaiusc != 'F')) {
             System.out.println("Sexo inválido :/");
-            break;
+            System.exit(0);
         }
-                
-        System.out.println("Sua altura em cm -> ")
-        int altura = read.nextInt;
 
-        System.out.printLn("Quantos anos você tem? -> ")
-        int idade = read.nextInt;
+        System.out.print("Informe seu peso em KG -> ");
+        double peso = read.nextDouble();
 
-        if (sexoMaiusc = 'M') {
-            System.out.print("Sua Taxa Metabólica Basal ---> ")
-            System.out.println(88,365 + (13,397 * peso) + (double)(4,799 * altura) - (double)(5,677 * idade));
-        } else if (sexoMaiusc = 'F') {
-            System.out.print("Sua Taxa Metabólica Basal ---> ")
-            System.out.println(447,593 + (9,247 * peso) + (double)(3,098 * altura) - (double)(4,330 * idade));
+        System.out.print("Sua altura em cm -> ");
+        int altura = read.nextInt();
+
+        System.out.print("Quantos anos você tem? -> ");
+        int idade = read.nextInt();
+
+        if (sexoMaiusc == 'M') {
+            System.out.print("Sua Taxa Metabólica Basal ---> ");
+            System.out.println(88.365 + (double)(13.397 * peso) + (double)(4.799 * altura) - (double)(5.677 * idade));
+        } else if (sexoMaiusc == 'F') {
+            System.out.print("Sua Taxa Metabólica Basal ---> ");
+            System.out.println(447.593 + (9.247 * peso) + (double)(3.098 * altura) - (double)(4.330 * idade));
         }
       
     }
