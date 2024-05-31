508c71e20f1330392d5b9c7447ef570fad714b77
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-31 20:00:00 -0300

    mediaMatriz.java

--- src/practicing/mediaMatriz.java
+++ src/practicing/mediaMatriz.java
@@ -2,16 +2,23 @@
 intervalo de 0 a 20. A seguir, informe:
 - média dos elementos da matriz abaixo de 5;
 - adicione em um Vetor V, os elementos da matriz acima da média;*/
+
+import java.util.Random;
+
 public class mediaMatriz {
 
     public static void main (String[] args) {
+
+        Random gerar = new Random();
 
         int[][] M = new int[4][4];
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                
+                M[i][j] = gere.nextInt(21);
+                System.out.print(M[i][j] + "\t")
             }
+            System.out.println();
         }
       
     }
