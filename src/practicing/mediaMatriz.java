7996bce7a655dc5b6769cee2b7fcb0dc78fe7a4d
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-31 19:56:02 -0300

    mediaMatriz.java

---
+++ src/practicing/mediaMatriz.java
@@ -0,0 +1,19 @@
+/*Elabore um programa que gere uma matriz 4x4. Os números devem ser gerados no
+intervalo de 0 a 20. A seguir, informe:
+- média dos elementos da matriz abaixo de 5;
+- adicione em um Vetor V, os elementos da matriz acima da média;*/
+public class mediaMatriz {
+
+    public static void main (String[] args) {
+
+        int[][] M = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                
+            }
+        }
+      
+    }
+  
+}
