422fa633542b601ec2d7072f396a6af9ec74b427
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-31 20:21:33 -0300

    mediaMatriz.java

--- src/practicing/mediaMatriz.java
+++ src/practicing/mediaMatriz.java
@@ -12,13 +12,45 @@ public class mediaMatriz {
         Random gerar = new Random();
 
         int[][] M = new int[4][4];
+        int[] V = new int[16];
+
+        int somaAbaixo5 = 0;
+        int contAbaixo5 = 0;
+        int contV = 0;
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                M[i][j] = gere.nextInt(21);
-                System.out.print(M[i][j] + "\t")
+                M[i][j] = gerar.nextInt(21);
+                System.out.print(M[i][j] + "\t");
+
+                if (M[i][j] <= 5) {
+                    somaAbaixo5 += M[i][j];
+                    contAbaixo5++;
+                }
+                
             }
             System.out.println();
+        }
+        
+        double media = (double)somaAbaixo5/contAbaixo5;
+        
+        System.out.println("Média dos elementos abaixo de 5 -> " + media);
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                if (M[i][j] >= media) {
+                    V[contV++] = M[i][j];
+                }
+                
+            }
+
+        }
+        
+        System.out.print("Elementos da Matriz que estão acima da média (" + media + ") -> ");
+        
+        for (int i = 0; i < contV; i++) {
+        	System.out.print(V[i] + "\t");
         }
       
     }
