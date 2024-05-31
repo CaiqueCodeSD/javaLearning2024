3e9713ba900737ca98e029e3ead7625487b1d82b
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-05-31 20:29:51 -0300

    notaAlunoMatriz.java
    A solução não está completa, vou continuar amanhã. Continuem acompanhando o processo!

---
+++ src/practicing/notaAlunoMatriz.java
@@ -0,0 +1,14 @@
+/*Elabore um programa que leia uma Matriz M (4x3) que representa as 03 notas de 04 alunos
+(cada linha representa um aluno e cada coluna representa uma nota). A seguir, adicione em
+Vetor V, a média das notas de cada aluno.*/
+
+public class notaAlunoMatriz {
+
+    public static void main (String[] args) {
+
+        int[][] M = new int[4][3];
+        int[] V = new int[4]; 
+      
+    }
+  
+}
