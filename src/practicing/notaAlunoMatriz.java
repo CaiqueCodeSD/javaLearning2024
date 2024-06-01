ddd940aa4f0e62702714385eb2c87a8d49f198b1
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-01 16:41:24 -0300

    notaAlunoMatriz.java
    A solução ainda não está completa.

--- src/practicing/notaAlunoMatriz.java
+++ src/practicing/notaAlunoMatriz.java
@@ -2,12 +2,24 @@
 (cada linha representa um aluno e cada coluna representa uma nota). A seguir, adicione em
 Vetor V, a média das notas de cada aluno.*/
 
+import java.util.Random;
+
 public class notaAlunoMatriz {
 
     public static void main (String[] args) {
 
+        Random gere = new Random();
+        
         int[][] M = new int[4][3];
         int[] V = new int[4]; 
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                M[i][j] = gere.nextInt(11);
+                System.out.print(M[i][j] + "\t")
+            }
+            System.out.println();
+        }
       
     }
   
