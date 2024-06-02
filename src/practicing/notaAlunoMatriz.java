71a78c59ddf166cd39253f735160326643b488d3
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-02 17:33:48 -0300

    notaAlunoMatriz.java
    Eu aprendi nesse código que é possível criar vetor do tipo double 
    :D

--- src/practicing/notaAlunoMatriz.java
+++ src/practicing/notaAlunoMatriz.java
@@ -1,4 +1,4 @@
-/*Elabore um programa que leia uma Matriz M (4x3) que representa as 03 notas de 04 alunos
+/*Elabore um programa que leia uma Matriz M (4x3) que representa as 03 notas de 04 estudantes
 (cada linha representa um aluno e cada coluna representa uma nota). A seguir, adicione em
 Vetor V, a média das notas de cada aluno.*/
 
@@ -11,14 +11,31 @@ public class notaAlunoMatriz {
         Random gere = new Random();
         
         int[][] M = new int[4][3];
-        int[] V = new int[4]; 
+        double[] V = new double[4]; 
+        
 
         for (int i = 0; i < 4; i++) {
+            System.out.print("Estudante [" + (i + 1) + "] -> " + "\t");
             for (int j = 0; j < 3; j++) {
                 M[i][j] = gere.nextInt(11);
-                System.out.print(M[i][j] + "\t")
+                System.out.print(M[i][j] + "\t");
             }
             System.out.println();
+        }
+
+        for (int i = 0; i < 4; i++) {
+           
+            for (int j = 0; j < 3; j++) {
+            	 V[i] += M[i][j];
+            }
+            V[i] = (V[i]/3);
+        }
+
+        System.out.println();
+        
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Estudante [" + (i + 1) + "] -> " + "\t");
+            System.out.println(V[i] + "\t");
         }
       
     }
