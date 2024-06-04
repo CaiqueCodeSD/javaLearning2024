2187325492692856dc02bef0d6cbd3a9aa72af78
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-03 21:02:30 -0300

    Create figurasGeometricas.java

---
+++ src/practicing/figurasGeometricas.java
@@ -0,0 +1,28 @@
+/*Em formato de menu, crie um programa para escolher uma figura geométrica plana (R, C,
+Q) e calcular a área e o perímetro da figura escolhida. Ao escolher o retângulo (R), o usuário
+deve informar o lado e altura. Ao escolher o círculo (C), o usuário deve informar o raio. Ao
+escolher quadrado (Q), o usuário deve escolher o lado. O sistema deve ser encerrado quando
+escolher a figura “Z”*/
+
+import java.util.Scanner;
+
+public class figurasGeometricas {
+
+    public static void main (String[] args) {
+
+        Scanner shape = new Scanner(System.in);
+
+        System.out.println("CALCULAR FIGURAS GEOMÉTICAS");
+        System.out.println("Rentângulo (R)");
+        System.out.println("Quadrado (Q)");
+        System.out.println("Círculo (C)");
+
+        System.out.println();
+        System.out.print("QUAL FIGURA DESEJA CALCULAR? -> ");
+        
+
+        
+      
+    }
+  
+}
