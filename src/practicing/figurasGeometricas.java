a68351db9c03ecc2a5505e5749183fbfc5259806
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-03 21:39:50 -0300

    Update figurasGeometricas.java

--- src/practicing/figurasGeometricas.java
+++ src/practicing/figurasGeometricas.java
@@ -1,8 +1,8 @@
 /*Em formato de menu, crie um programa para escolher uma figura geométrica plana (R, C,
-Q) e calcular a área e o perímetro da figura escolhida. Ao escolher o retângulo (R), o usuário
+Q) e calcular a área da figura escolhida. Ao escolher o retângulo (R), o usuário
 deve informar o lado e altura. Ao escolher o círculo (C), o usuário deve informar o raio. Ao
 escolher quadrado (Q), o usuário deve escolher o lado. O sistema deve ser encerrado quando
-escolher a figura “Z”*/
+escolher a figura 'Z'*/
 
 import java.util.Scanner;
 
@@ -12,29 +12,60 @@ public class figurasGeometricas {
 
         Scanner shape = new Scanner(System.in);
 
-        System.out.println("CALCULAR FIGURAS GEOMÉTICAS");
+        System.out.println("CALCULAR FIGURAS GEOMÉTRICAS");
         System.out.println("Rentângulo (R)");
         System.out.println("Quadrado (Q)");
         System.out.println("Círculo (C)");
 
         System.out.println();
         System.out.print("QUAL FIGURA DESEJA CALCULAR? -> ");
-        char forma = read.next().charAt(0);
+        char forma = shape.next().charAt(0);
         char formaMaiusc = Character.toUpperCase(forma);
         
-        while (formaMaiusc == 'R' || formaMaiusc == 'Q' || formaMaiusc == 'C') {
+      
+        while (formaMaiusc != 'Z') {
+            
+	        switch (formaMaiusc) {
+	     	case 'R': 
+	     		 System.out.println("INSIRA OS VALORES: ");
+	     	     System.out.print("Altura -> ");
+	     		 double a = shape.nextDouble();
+	     		 System.out.print("Base -> ");
+	     		 double b = shape.nextDouble();
+	     		 System.out.println("Área do retângulo: " + (a*b));
+	     		 System.out.println();
+	     			  break;
+	     	 case 'C':
+	     		 System.out.println("INSIRA OS VALORES: ");
+	             System.out.print("Informe o raio -> ");
+	             double raio = shape.nextDouble();
+	             double areaCirculo = Math.PI * Math.pow(raio, 2);
+	             double perimetroCirculo = 2 * Math.PI * raio;
+	             System.out.println("Área do círculo -> " + (Math.PI * Math.pow(raio, 2)));
+	             System.out.println();
+	             break;
+	         case 'Q':
+	     		 System.out.println("INSIRA OS VALORES: ");
+	             System.out.print("Informe o lado -> ");
+	             double lado = shape.nextDouble();
+	             System.out.println("Área do quadrado -> " + (lado * lado));
+	             System.out.println();
+	             break;
+	         case 'Z':
+	             System.out.println("[PROGRAMA ENCERRADO]");
+	             break;
+	         default:
+	             System.out.println("Opção inválida!");
+	             System.out.println();
+	     		 break;
+	     		}
+	        
+	        System.out.print("QUAL FIGURA DESEJA CALCULAR? -> ");
+            forma = shape.next().charAt(0);
+            formaMaiusc = Character.toUpperCase(forma);
             
         }
-
-        if (formaMaius != 'R' || formaMaiusc != 'Q' || formaMaiusc != 'C') {
-            System.out.println("Opção Inválida!")
-            System.exit(0);
-        }
-
-        switch (formaMaiusc) {
-            case R: 
-        }
-      
+        
     }
   
 }
