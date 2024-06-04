f5aca9e3ee94950cd1fd6affd04347164c899779
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-04 20:59:38 -0300

    Create esporteFavorito.java

---
+++ src/practicing/esporteFavorito.java
@@ -0,0 +1,6 @@
+/*Elabore um programa para controlar o esporte e time preferido de um conjunto de
+pessoas. O programa deverá ler o esporte preferido (F - Futebol, V - Vôlei, B - Basquete), o time
+que o mesmo torce e o sexo (F, M) do entrevistado. O programa deverá ser encerrado com o
+esporte “Z”. A seguir, informe: o percentual de preferência do futebol; o percentual das
+mulheres que preferem vôlei e o percentual de pessoas, dentre os que preferem futebol, que
+torcem pelo RMD.*/
