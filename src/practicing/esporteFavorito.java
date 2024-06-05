77cb80dce20b6c7e334e5432c4e61a351dfe24f2
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-05 20:09:19 -0300

    Update esporteFavorito.java
    continuo respondendo, em breve trarei mais atualizações!
    
    :)

--- src/practicing/esporteFavorito.java
+++ src/practicing/esporteFavorito.java
@@ -3,7 +3,7 @@ pessoas. O programa deverá ler o esporte preferido (F - Futebol, V - Vôlei, B 
 que o mesmo torce e o sexo (F, M) do entrevistado. O programa deverá ser encerrado com o
 esporte “Z”. A seguir, informe: o percentual de preferência do futebol; o percentual das
 mulheres que preferem vôlei e o percentual de pessoas, dentre os que preferem futebol, que
-torcem pelo RMD.*/
+torcem pelo time 1.*/
 
 import java.util.Scanner;
 
@@ -12,21 +12,42 @@ public class esporteFavorito {
     public static void main (String[] args) {
 
         Scanner read = new Scanner(System.in);
+        
+        char esporteMaiusc = 'A';
+        int entrevistado = 0;
+        
+        System.out.println("ESPORTES");
+        System.out.println("[F] FUTEBOL - [V] Vôlei - [B] Basquete");
+        System.out.println("TIMES (de 1 a 10)");
 
-        System.out.print("Quantas pessoas participarão da pesquisa?");
-        int quant = read.nextInt();
-
-        while (esporte != 'Z') {
+        while (esporteMaiusc != 'Z') {
+        	
+        	System.out.println("Entrevistado " + (entrevistado +1) + ": ");
 
             System.out.print("ESPORTE FAVORITO -> ");
             char esporte = read.next().charAt(0);
-            char esporteMaiusc = Character.toUpperCase(esporte);
+            esporteMaiusc = Character.toUpperCase(esporte);
+            
+			if (esporteMaiusc == 'Z') {
+				System.out.println("PROGRAMA ENCERRADO");
+				System.exit(0);
+			}
+			      
 
-            System.out.println("SEXO DO ENTREVISTADO -> ");
+            System.out.print("SEXO DO ENTREVISTADO -> ");
             char sexo = read.next().charAt(0);
             char sexoMaiusc = Character.toUpperCase(sexo);
-
             
+            if (sexoMaiusc != 'M' && sexoMaiusc != 'F') {
+				System.out.println("SEXO INVÁLIDO");
+			}
+
+            System.out.print("TORCE PARA O TIME -> ");
+            int time = read.nextInt();
+            
+            System.out.println();
+            
+            entrevistado++;
             
         }
       
