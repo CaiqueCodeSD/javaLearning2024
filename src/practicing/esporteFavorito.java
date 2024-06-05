bbb5ec7aaa18018f3f5397f3ab24afb2cfdeecb9
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-05 17:52:12 -0300

    Update esporteFavorito.java
    Mais tarde eu finalizo o código.

--- src/practicing/esporteFavorito.java
+++ src/practicing/esporteFavorito.java
@@ -4,3 +4,15 @@ que o mesmo torce e o sexo (F, M) do entrevistado. O programa deverá ser encerr
 esporte “Z”. A seguir, informe: o percentual de preferência do futebol; o percentual das
 mulheres que preferem vôlei e o percentual de pessoas, dentre os que preferem futebol, que
 torcem pelo RMD.*/
+
+import java.util.Scanner;
+
+public class esporteFavorito {
+
+    public static void main (String[] args) {
+
+        
+      
+    }
+  
+}
