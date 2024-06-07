a0ece910c2fbdb59d51d21c3935da9431ec51b8c
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-06 21:19:20 -0300

    Create vendasNaSemana.java
    Eu pretendo resolver essa questão amanhã. Estou ocupado resolvendo outra situação.

---
+++ src/practicing/vendasNaSemana.java
@@ -0,0 +1,7 @@
+/*Gere uma matriz 20x7 que representa a quantidade de produtos vendidos (0 a 100) de 20
+produtos em uma determinada semana (cada coluna representa um dia na semana (0 –
+domingo, 1 – segunda, .....)). A seguir, informe:
+- o produto com mais venda na semana;
+- o produto com mais venda no fim de semana (sábado e domingo);
+- adicione em um vetor V as vendas dos produtos de segunda à quarta (3 dias
+acumulados);*/
