4ef242936283fcd64bc46aff1a4525d13c6e3b85
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-06-13 19:55:04 -0300

    Create terceiraMatriz.java

---
+++ src/matriz/terceiraMatriz.java
@@ -0,0 +1,15 @@
+/*Escreva um programa em Java que some duas matrizes de tamanho 3x3 e armazene o resultado em uma terceira matriz.*/
+
+public class terceiraMatriz {
+
+    public static void main(String[] args) {
+
+        int[][] M1 = new int[3][3];
+        int[][] M2 = new int[3][3];
+        int[][] M3 = new int[3][3];
+
+        
+      
+    }
+  
+}
