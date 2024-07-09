befc1994038c5c28b39657dc3a3dab7f50c5819a
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-07-09 20:26:19 -0300

    Create Main.java

---
+++ src/methodsLearning/bank2/Main.java
@@ -0,0 +1,13 @@
+package Bank2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Account chrisAccount = new Account("15487", 868469.57, "Chris", "chris@chris", "9985478547");
+		
+		chrisAccount.showAccount();
+		
+	}
+	
+}
