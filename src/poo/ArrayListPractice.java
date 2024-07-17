25253bcc5625b043b1185d03f63a00c15f17f1f1
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-07-17 18:59:43 -0300

    rename ArrayListPractice to ArrayListPractice.java

--- src/poo/ArrayListPractice.java
+++ src/poo/ArrayListPractice.java
@@ -6,16 +6,16 @@ public class Main {
     ArrayList<Integer> numeros = new ArrayList<Integer>();
 
     numeros.add(61891);
+    numeros.add(67871);
+    numeros.add(636681);
 
     Collections.sort(numeros);
 
     int i = 0;
 
-    for(Integer num : numeros) {
-
+    for (Integer num : numeros) {
         System.out.println("No index [" + i + "] temos o número: " + num);
         i++;
-
     }
  
 }
