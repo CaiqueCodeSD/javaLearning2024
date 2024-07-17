f60cfa7dc0282ffca48aee2e6dd70354d011e772
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-07-17 20:46:38 -0300

    Create Produto.java

---
+++ src/arrayList/Produto.java
@@ -0,0 +1,46 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class Produto {
+
+	String nome;
+	double preco;
+	
+	public Produto (String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Produto> produto = new ArrayList<Produto>();
+		produto.add(new Produto("Gabinete", 450.15));
+		produto.add(new Produto("Teclado", 110.99));
+		produto.add(new Produto("Mouse", 80.75));
+		produto.add(new Produto("Monitor", 1100.20));
+		produto.add(new Produto("Placa de Vídeo", 800));
+		
+		for (Produto prod : produto) {
+			System.out.println(prod.getNome() + " custa R$" + prod.getPreco());
+		}
+		
+	}
+	
+}
