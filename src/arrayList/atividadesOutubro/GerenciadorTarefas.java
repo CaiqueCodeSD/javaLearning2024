e9e6684cb24ba04cf74f1d65df17289b34f91308
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-10-09 20:56:25 -0300

    Create GerenciadorTarefas.java

---
+++ src/arrayList/atividadesOutubro/GerenciadorTarefas.java
@@ -0,0 +1,42 @@
+package arrayList.atividadesOutubro;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GerenciadorTarefas {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		Tarefa tarefa = new Tarefa(null, null);
+		ArrayList<String> tarefas = new ArrayList<>();
+		
+		MenuGerenciadorTarefas.mostrarMenu();
+		int opcao = entrada.nextInt();
+		
+		while (opcao > 0) {
+			switch (opcao) {
+			case 1:
+				System.out.print("Nome da tarefa: ");
+				String nome = entrada.next();
+				tarefa.setNome(nome);
+				System.out.print("Prioridade da tarefa: ");
+				String prioridade = entrada.next();
+				tarefa.setNome(prioridade);
+				
+				tarefas.add(nome);
+				
+				break;
+
+			default:
+				break;
+			}
+			MenuGerenciadorTarefas.mostrarMenu();
+			opcao = entrada.nextInt();
+		}
+		
+		System.out.println(tarefas);
+		
+	}
+	
+}
