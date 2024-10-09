fe6124b6679e036f000158664c2f46623c37745a
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-10-09 20:55:31 -0300

    Create MenuGerenciadorTarefas.java

---
+++ src/arrayList/atividadesOutubro/MenuGerenciadorTarefas.java
@@ -0,0 +1,17 @@
+package arrayList.atividadesOutubro;
+
+public abstract class MenuGerenciadorTarefas {
+
+	public MenuGerenciadorTarefas() {
+		
+	}
+	
+	public static void mostrarMenu() {
+		System.out.println("GERENCIADOR DE TAREFAS\n"
+						+ "[1] Adicionar\n"
+						+ "[2] Remover\n"
+						+ "[3] Listar\n"
+						+ "[0] Sair\n");
+	}
+	
+}
